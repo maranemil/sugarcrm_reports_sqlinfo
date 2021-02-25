7919ceeb563ecfafe4e9015e7a32c6af20f71d98
maranemil <maran.emil@gmail.com>
2021-02-25 20:42:47 +0100

    reformatted code

--- manifest.php
+++ manifest.php
@@ -7,49 +7,49 @@
  */
 
 $manifest = array(
-    array(
-        'acceptable_sugar_versions' =>
-            array(
-                'regex_matches' => array(
-                    '6.5.*',
-                    '7.5.*',
-                ),
-                /*'exact_matches' => array(
-                    '6.5.17',
-                    '6.5.16',
-                ),*/
-            )
-    ),
-    array(
-        'acceptable_sugar_flavors' =>
-            array(
-                'CE',
-                'PRO',
-                'CORP',
-                'ENT',
-                'ULT',
-            ),
-    ),
-    'readme' => 'Show SQL Query for Sugar Reports',
-    'key' => 'ext',
-    'author' => 'Emil Maran',
-    'description' => 'Show SQL Query for Sugar Reports',
-    'icon' => '',
-    'is_uninstallable' => true,
-    'name' => 'ShowSQLQuerySugarReports',
-    'published_date' => '2015-04-29 14:22:00',
-    'type' => 'module',
-    'version' => '0.1',
-    //'remove_tables' => 'prompt',
+	array(
+		'acceptable_sugar_versions' =>
+			array(
+				'regex_matches' => array(
+					'6.5.*',
+					'7.5.*',
+				),
+				/*'exact_matches' => array(
+					'6.5.17',
+					'6.5.16',
+				),*/
+			)
+	),
+	array(
+		'acceptable_sugar_flavors' =>
+			array(
+				'CE',
+				'PRO',
+				'CORP',
+				'ENT',
+				'ULT',
+			),
+	),
+	'readme'           => 'Show SQL Query for Sugar Reports',
+	'key'              => 'ext',
+	'author'           => 'Emil Maran',
+	'description'      => 'Show SQL Query for Sugar Reports',
+	'icon'             => '',
+	'is_uninstallable' => true,
+	'name'             => 'ShowSQLQuerySugarReports',
+	'published_date'   => '2015-04-29 14:22:00',
+	'type'             => 'module',
+	'version'          => '0.1',
+	//'remove_tables' => 'prompt',
 );
 
 $installdefs = array(
-    'id' => 'SQSREP2',
-    'copy' =>
-        array(
-            array(
-                'from' => '<basepath>/modules/Reports/Report.php',
-                'to' => 'modules/Reports/Report.php',
-            ),
-        ),
+	'id'   => 'SQSREP2',
+	'copy' =>
+		array(
+			array(
+				'from' => '<basepath>/modules/Reports/Report.php',
+				'to'   => 'modules/Reports/Report.php',
+			),
+		),
 );
